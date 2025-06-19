37a96e66af5f832ca3168be23680a4272ad37784
Kristian Pacheco <kristianpachecar18@gmail.com>
2025-06-19 23:57:55 +0000

    Added some logic for sign up view

--- src/front/pages/SignUp.jsx
+++ src/front/pages/SignUp.jsx
@@ -1,2 +1,29 @@
 import { useState } from "react";
 
+export function SignUp() {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        is_admin: false
+    });
+
+    const handleChange = (e) => {
+        const { name, value, type, checked } = e.target;
+        setFormData({
+            ...formData,
+            [name]: type === "checkbox" ? checked : value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const resp = await fetch("", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(formData)
+            })
+        }
+    };
+};
