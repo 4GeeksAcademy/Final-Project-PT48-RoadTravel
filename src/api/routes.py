f1d3dc46da541441a22b4d709798c1322d3ecf20
Kristian Pacheco <kristianpachecar18@gmail.com>
2025-06-28 15:44:09 +0000

    Added logic to endpoint PUT for editing reservatrion dates, still missing some date conflict authentication

--- src/api/routes.py
+++ src/api/routes.py
@@ -190,14 +190,14 @@ def make_reservation():
 @api.route('/my-reservation/<int:id>', methods=['GET'])
 @jwt_required()
 def get_reservation(id):
-    user_id = get_jwt_identity
+    user_id = get_jwt_identity()
     reservation = Booking.query.get(id)
 
     if not reservation:
         return jsonify({'msg': 'No reservation listed'}), 404
 
     if user_id != reservation.user_id:
-        return jsonify({'msg': 'No authorized to see reservation'}), 401
+        return jsonify({'msg': 'No authorized to see reservation'}), 403
 
     return jsonify(Booking.serialize()), 200
 
@@ -205,26 +205,43 @@ def get_reservation(id):
 @api.route('/my-reservation/<int:id>', methods=['PUT'])
 @jwt_required()
 def edit_reservation(id):
+    user_id = get_jwt_identity()
     data = request.get_json()
+    reservation = Booking.query.get(id)
+    if not reservation:
+        return jsonify({'msg': 'No reservation listed'}), 404
+    
+    if user_id != reservation.user_id:
+        return jsonify({'msg': 'No authorized to edit reservation'}), 403
+
     if not data:
         return jsonify({'msg': 'No data was edited'}), 400
-
-    reserved_car = Booking.query.get(id)
-    reserved_car.start_day = data['start_day']
-    reserved_car.end_day = data['end_day']
-
+    
+    start_day = data.get('start_day')
+    end_day = data.get('end_day')
+    if not start_day or not end_day:
+        return jsonify({'msg': 'Missing start day or end day'}), 400
+    
+    try:
+        start_day_obj = datetime.strptime(start_day, '%Y-%m-%d').date()
+        end_day_obj = datetime.strptime(end_day, '%Y-%m-%d').date()
+    except ValueError:
+        return jsonify({'msg': 'Invalid date format'}), 400
+    
+    if start_day_obj > end_day_obj:
+        return jsonify({'msg': 'Start day must be before end day'}), 400
 
 @api.route('/my-reservation/<int:id>', methods=['DELETE'])
 @jwt_required()
 def delete_reservation(id):
-    user_id = get_jwt_identity
+    user_id = get_jwt_identity()
     reservation = Booking.query.get(id)
 
     if not reservation:
         return jsonify({'msg': 'No reservation listed'}), 404
 
     if user_id != reservation.user_id:
-        return jsonify({'msg': 'No authorized to remove reservation'}), 401
+        return jsonify({'msg': 'No authorized to delete reservation'}), 403
     db.session.delete(reservation)
     db.session.commit()
 
