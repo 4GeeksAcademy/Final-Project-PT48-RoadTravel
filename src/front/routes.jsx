63ba74225cabfd0d3231e57e04eee134685f0f93
Kristian Pacheco <kristianpachecar18@gmail.com>
2025-07-04 18:31:02 +0000

    Fixed route for my reservations

--- src/front/routes.jsx
+++ src/front/routes.jsx
@@ -10,7 +10,7 @@ import DescriptionMediumCar from "./pages/DescriptionMediumCar.jsx";
 import DescriptionPremiumCar from "./pages/DescriptionPremiumCar.jsx";
 import MakeReservation from "./pages/MakeReservation.jsx"
 import Booking from "./pages/Booking.jsx"
-import MyReservations from './views/MyReservations.jsx';
+import MyReservations from './pages/MyReservations.jsx';
 
 
 export const router = createBrowserRouter([
